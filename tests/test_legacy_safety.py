4d8bd8fa8127561db859f658aa597eb2ed31a476
Hiago Pinho <57301090+HPinho@users.noreply.github.com>
2026-09-04 13:04:48 -0300

    tests: register external IRQ module

--- tests/test_legacy_safety.py
+++ tests/test_legacy_safety.py
@@ -25,6 +25,7 @@ class LegacySafetyTests(unittest.TestCase):
         "kernel/src/acpi/hpet.sotlas", "kernel/src/acpi/fadt.sotlas",
         "kernel/src/acpi/pm_timer.sotlas", "kernel/src/interrupts/route.sotlas",
         "kernel/src/interrupts/lapic.sotlas", "kernel/src/interrupts/ioapic.sotlas",
+        "kernel/src/interrupts/irq.sotlas",
         "kernel/src/drivers/display_driver.sotlas",
         "kernel/src/drivers/pci_bus.sotlas", "kernel/src/drivers/storage_discovery.sotlas",
         "kernel/src/drivers/xhci_discovery.sotlas", "kernel/src/drivers/xhci_ring.sotlas",
