ef2b0c3a4fe7771f729188309c61b814db838c4c
Hiago Pinho <57301090+HPinho@users.noreply.github.com>
2026-09-04 18:09:43 -0300

    tests: register xhci enable-slot module

--- tests/test_legacy_safety.py
+++ tests/test_legacy_safety.py
@@ -34,6 +34,7 @@ class LegacySafetyTests(unittest.TestCase):
         "kernel/src/drivers/xhci_start.sotlas", "kernel/src/drivers/xhci_command.sotlas",
         "kernel/src/drivers/xhci_port.sotlas", "kernel/src/drivers/xhci_protocol.sotlas",
         "kernel/src/drivers/xhci_port_reset.sotlas", "kernel/src/drivers/xhci_port_stage.sotlas",
+        "kernel/src/drivers/xhci_slot.sotlas",
         "kernel/src/drivers/xhci_ring.sotlas", "kernel/src/drivers/xhci_trb.sotlas",
         "kernel/src/drivers/xhci_event.sotlas", "kernel/src/drivers/xhci_erst.sotlas",
         "kernel/src/drivers/xhci_event_ring.sotlas",
