c985a12c62f20ce9c9a28772bd7443d02cc0aef4
Hiago Pinho <57301090+HPinho@users.noreply.github.com>
2026-09-04 17:45:38 -0300

    tests: register xhci port inventory module

--- tests/test_legacy_safety.py
+++ tests/test_legacy_safety.py
@@ -31,7 +31,7 @@ class LegacySafetyTests(unittest.TestCase):
         "kernel/src/drivers/pci_bus.sotlas", "kernel/src/drivers/storage_discovery.sotlas",
         "kernel/src/drivers/xhci_discovery.sotlas", "kernel/src/drivers/xhci_controller.sotlas",
         "kernel/src/drivers/xhci_runtime.sotlas", "kernel/src/drivers/xhci_program.sotlas",
-        "kernel/src/drivers/xhci_start.sotlas",
+        "kernel/src/drivers/xhci_start.sotlas", "kernel/src/drivers/xhci_port.sotlas",
         "kernel/src/drivers/xhci_ring.sotlas", "kernel/src/drivers/xhci_trb.sotlas",
         "kernel/src/drivers/xhci_event.sotlas", "kernel/src/drivers/xhci_erst.sotlas",
         "kernel/src/drivers/xhci_event_ring.sotlas",
