557a5dae6218af26351bdeaaf564c189c1d22c27
Hiago Pinho <57301090+HPinho@users.noreply.github.com>
2026-09-04 19:14:58 -0300

    tests: register xhci configure endpoint module

--- tests/test_legacy_safety.py
+++ tests/test_legacy_safety.py
@@ -38,7 +38,7 @@ class LegacySafetyTests(unittest.TestCase):
         "kernel/src/drivers/xhci_slot.sotlas", "kernel/src/drivers/xhci_context.sotlas",
         "kernel/src/drivers/xhci_address.sotlas", "kernel/src/drivers/xhci_ep0.sotlas",
         "kernel/src/drivers/xhci_descriptor.sotlas", "kernel/src/drivers/xhci_configuration.sotlas",
-        "kernel/src/drivers/xhci_hid_context.sotlas",
+        "kernel/src/drivers/xhci_hid_context.sotlas", "kernel/src/drivers/xhci_configure_endpoint.sotlas",
         "kernel/src/drivers/xhci_ring.sotlas", "kernel/src/drivers/xhci_trb.sotlas",
         "kernel/src/drivers/xhci_event.sotlas", "kernel/src/drivers/xhci_erst.sotlas",
         "kernel/src/drivers/xhci_event_ring.sotlas",
