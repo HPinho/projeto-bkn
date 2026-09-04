59fd0d33374a1eb6e42353851a37d767de8e5ae6
Hiago Pinho <57301090+HPinho@users.noreply.github.com>
2026-09-04 17:50:43 -0300

    tests: register xhci protocol module

--- tests/test_legacy_safety.py
+++ tests/test_legacy_safety.py
@@ -32,6 +32,7 @@ class LegacySafetyTests(unittest.TestCase):
         "kernel/src/drivers/xhci_discovery.sotlas", "kernel/src/drivers/xhci_controller.sotlas",
         "kernel/src/drivers/xhci_runtime.sotlas", "kernel/src/drivers/xhci_program.sotlas",
         "kernel/src/drivers/xhci_start.sotlas", "kernel/src/drivers/xhci_port.sotlas",
+        "kernel/src/drivers/xhci_protocol.sotlas",
         "kernel/src/drivers/xhci_ring.sotlas", "kernel/src/drivers/xhci_trb.sotlas",
         "kernel/src/drivers/xhci_event.sotlas", "kernel/src/drivers/xhci_erst.sotlas",
         "kernel/src/drivers/xhci_event_ring.sotlas",
