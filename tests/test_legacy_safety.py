b299d1ae7caf07245d7fcc3e23a2547dc851034d
Hiago Pinho <57301090+HPinho@users.noreply.github.com>
2026-09-04 08:31:04 -0300

    tests: register canonical xHCI discovery module

--- tests/test_legacy_safety.py
+++ tests/test_legacy_safety.py
@@ -26,6 +26,7 @@ class LegacySafetyTests(unittest.TestCase):
         "kernel/src/acpi/pm_timer.sotlas", "kernel/src/interrupts/route.sotlas",
         "kernel/src/drivers/display_driver.sotlas",
         "kernel/src/drivers/pci_bus.sotlas", "kernel/src/drivers/storage_discovery.sotlas",
+        "kernel/src/drivers/xhci_discovery.sotlas",
         "kernel/src/drivers/i8042.sotlas", "kernel/src/drivers/ps2_mouse.sotlas",
         "kernel/src/drivers/ps2_keyboard.sotlas",
         "kernel/src/storage/block_device.sotlas", "kernel/src/storage/gpt.sotlas",
